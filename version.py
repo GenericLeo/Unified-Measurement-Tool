8f4f90163421782ab86887cd964600117d97f5ae
lgsoler <lgsoler@ncsu.edu>
2026-03-30 19:56:20 -0400

    Bump version to 1.0.0

--- version.py
+++ version.py
@@ -1,5 +1,5 @@
 """Application version metadata."""
 
-__version__ = "0.1.0"
+__version__ = "1.0.0"
 __app_name__ = "Unified Measurement Tool"
 __github_repo__ = "GenericLeo/Unified-Measurement-Tool"
